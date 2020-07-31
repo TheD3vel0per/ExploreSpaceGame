5c466ca9a2800def2ee1804f17a4cbd27c28453a
Devam Sisodraker <devam@sisodraker.com>
2020-07-31 14:09:23 -0700

    Co-Authored by: Aiden Kerr <arkerr2000@gmail.com>
    Made level 1 scene and added realistic proportions for mass for level 1

--- Assets/Scripts/Gravity/GravityManager.cs
+++ Assets/Scripts/Gravity/GravityManager.cs
@@ -10,42 +10,48 @@ public class GravityManager : MonoBehaviour {
 
         var G = GravityConfig.gravitationalConstant;
 
-        foreach (var targetA in targets)
-        foreach (var targetB in targets) {
-            // Getting the RigidBody
-            var rbA = targetA.gameObject.GetComponent<Rigidbody>();
-            var rbB = targetB.gameObject.GetComponent<Rigidbody>();
+        foreach (var targetA in targets) {
+            Vector3 acceleration = Vector3.zero;
+            foreach (var targetB in targets) {
+                // Getting the RigidBody
+                var rbA = targetA.gameObject.GetComponent<Rigidbody>();
+                var rbB = targetB.gameObject.GetComponent<Rigidbody>();
 
-            // Not the same rigidBody 0/0 = NaN !!!
-            if (rbA != rbB) {
-                // Mass of A
-                var massA = rbA.mass;
+                // Not the same rigidBody 0/0 = NaN !!!
+                if (rbA != rbB) {
+                    // Mass of A
+                    var massA = rbA.mass;
 
-                // Mass of B
-                var massB = rbB.mass;
+                    // Mass of B
+                    var massB = rbB.mass;
 
-                // Calculating Distance between Centre of Gravity as a Scalar
-                var r = Vector3.Distance(
-                    rbA.gameObject.transform.position,
-                    rbB.gameObject.transform.position
-                );
+                    // Calculating Distance between Centre of Gravity as a Scalar
+                    var r = Vector3.Distance(
+                        rbA.gameObject.transform.position,
+                        rbB.gameObject.transform.position
+                    );
 
-                // Gets a vector that points from the player's position to the target's.
-                var heading = rbB.gameObject.gameObject.transform.position -
-                              rbA.gameObject.gameObject.transform.position;
-                var distance = heading.magnitude;
-                var direction = heading / distance; // This is now the normalized direction.
+                    // Gets a vector that points from the player's position to the target's.
+                    var heading = rbB.gameObject.gameObject.transform.position -
+                                  rbA.gameObject.gameObject.transform.position;
+                    var distance = heading.magnitude;
+                    var direction = heading / distance; // This is now the normalized direction.
 
-                // Drum roll calculation for gravitational force
-                var force = direction * (-1 * (massA * massB * GravityConfig.gravitationalConstant) /
-                                         (distance * distance));
+                    // Drum roll calculation for gravitational force
+                    var force = direction * (-1 * (massA * massB * GravityConfig.gravitationalConstant) /
+                                             (distance * distance));
 
-                // Update the position
-                targetB.UpdatePosition(
-                    targetB.UpdateVelocity(force / massB, timeStep),
-                    timeStep
-                );
+
+                    Vector3 forceDir = (targetB.gameObject.transform.position - targetA.gameObject.transform.position).normalized;
+                    float sqrDst = (targetB.gameObject.transform.position - targetA.gameObject.transform.position).sqrMagnitude;
+                    acceleration += forceDir * GravityConfig.gravitationalConstant * targetB.mass / sqrDst;
+                }
             }
+            // Update the position
+            targetA.UpdatePosition(
+                targetA.UpdateVelocity(acceleration, timeStep),
+                timeStep
+            );
         }
     }
 }
