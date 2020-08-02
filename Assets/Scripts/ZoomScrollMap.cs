a87ed85707f45a0d6642bd89aaa2a168dc7f6669
Devam Sisodraker <devam@sisodraker.com>
2020-08-01 17:22:05 -0700

    You can zoom onto your rocket ship from the top down view

---
+++ Assets/Scripts/ZoomScrollMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomScrollMap : MonoBehaviour {
+    private Camera _topCamera;
+
+    // Start is called before the first frame update
+    void Start() {
+        _topCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.E))
+            _topCamera.orthographicSize -= 0.75f;
+        if (Input.GetKey(KeyCode.Q))
+            _topCamera.orthographicSize += 0.75f;
+    }
+}
